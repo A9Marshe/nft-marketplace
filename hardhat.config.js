78d8520f96c3732eea41108e3c50fd0b385422d7
Alex <alkhateeb.alex@hotmail.com>
2022-10-01 14:40:44 +0300

    added quick test for deployment and minting tokens

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,3 @@
-require("@nomicfoundation/hardhat-toolbox");
 require("@nomiclabs/hardhat-waffle");
 
 /** @type import('hardhat/config').HardhatUserConfig */
@@ -6,21 +5,21 @@ module.exports = {
   defaultNetwork: "hardhat",
   networks: {
     hardhat: {
-      chainId: 1337
+      chainId: 1337,
     },
-//  unused configuration commented out for now
-//  mumbai: {
-//    url: "https://rpc-mumbai.maticvigil.com",
-//    accounts: [process.env.privateKey]
-//  }
+    //  unused configuration commented out for now
+    //  mumbai: {
+    //    url: "https://rpc-mumbai.maticvigil.com",
+    //    accounts: [process.env.privateKey]
+    //  }
   },
   solidity: {
     version: "0.8.4",
     settings: {
       optimizer: {
         enabled: true,
-        runs: 200
-      }
-    }
-  }
+        runs: 200,
+      },
+    },
+  },
 };
