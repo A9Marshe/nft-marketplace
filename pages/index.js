4d907a737dfb89ec183baee7af2aece33fd38d45
Alex <alkhateeb.alex@hotmail.com>
2022-10-08 00:01:22 +0300

    fixed config.js reference (again)

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import Web3Modal from "web3modal";
 
-import { marketplaceAddress } from "../config";
+import { marketplaceAddress } from "../config.js";
 
 import NFTMarketplace from "../artifacts/contracts/NFTMarketplace.sol/NFTMarketplace.json";
 
